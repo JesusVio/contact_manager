1ca03f8e20a5a677e4ed86d80f0a2b72b8e68cf0
Jesus Violante <jv1108434@student.francistuttle.edu>
2024-01-30 09:44:25 -0600

    working version of contact-manager

--- src/index.js
+++ src/index.js
@@ -9,9 +9,6 @@ export default function Banner() {
 
 function Contact() {
   const [contacts, setContacts] = useState([]);
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
-  //const [email, updateEmail] = useState("");
 
   const Add = () => {
     const inputName = window.prompt("Enter a name: ");
@@ -22,25 +19,60 @@ function Contact() {
         setContacts([...contacts, newContact]);
       }
     }
-
-    return (
-      <div>
-        <ul>
-          {contacts.map((contact, index) => (
-            <li key={index}>
-              Name: {contact.name} | Phone: {contact.phone}
-            </li>
-          ))}
-        </ul>
-        <button onClick={Add}>Add</button>
-      </div>
-    );
   };
+
+  const Delete = () => {
+    const deletePhone = window.prompt("Enter Phone Number to DELETE: ");
+    if (deletePhone) {
+      const updatedContacts = contacts.filter(
+        (contact) => contact.phone !== deletePhone
+      );
+      setContacts(updatedContacts);
+    }
+  };
+
+  const Update = () => {
+    const updatePhone = window.prompt("Enter Phone to Update");
+    if (updatePhone) {
+      const newNumber = window.prompt("Enter New Number: ");
+      if (newNumber !== null) {
+        const updatedContacts = contacts.map((contact) =>
+          contact.phone === updatePhone
+            ? { ...contact, phone: newNumber }
+            : contact
+        );
+        setContacts(updatedContacts);
+      }
+    }
+  };
+
+  return (
+    <div>
+      <ol className="list">
+        {contacts.map((contact, index) => (
+          <li key={index}>
+            Name: {contact.name} | Phone: {contact.phone}
+          </li>
+        ))}
+      </ol>
+      <div className="button-container">
+        <button className="button" onClick={Add}>
+          Add
+        </button>
+        <button className="button" onClick={Delete}>
+          Delete
+        </button>
+        <button className="button" onClick={Update}>
+          Update
+        </button>
+      </div>
+    </div>
+  );
 }
 
 root.render(
   <>
-    <div>
+    <div className="body">
       <Banner />
       <Contact />
     </div>
